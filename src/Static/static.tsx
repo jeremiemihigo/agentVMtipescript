ee97e9e4694b3c95f53f6950c5968d0a0f1ca9af
unknown <grace1mihigo@gmail.com>
2024-11-11 11:54:01 +0200

    lisod

--- src/Static/static.tsx
+++ src/Static/static.tsx
@@ -12,8 +12,8 @@ import { IDeedline, IDelai, IRaison, ISat } from "../Interface/IStatic";
 const link = "https://visitetwo.bboxxvm.com";
 //const link = "http://localhost:60000";
 //export const big_data = "https://issuelink.bboxxvm.com/bboxx/support";
-export const big_data = "http://localhost:60000/bboxx/support";
-//export const big_data = "https://visitetwo.bboxxvm.com/bboxx/support";
+//export const big_data = "http://localhost:60000/bboxx/support";
+export const big_data = "https://visitetwo.bboxxvm.com/bboxx/support";
 
 export const lien = `${link}/bboxx/support`;
 export const lien_socket = link;
